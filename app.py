bf503c63d0ffe454e6e163d656a91b2a27dd51a3
ShreyaPatil555 <shreyapatil0811@gmail.com>
2025-08-29 13:02:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "🚀 Hello from Wisecow Flask App running in Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
